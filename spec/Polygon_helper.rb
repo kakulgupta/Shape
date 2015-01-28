c9398b7eab1678ff5a803796e1ff565e32254466
KAKUL GUPTA <kakul.gupta009@gmail.com>
2015-01-28 14:08:46 +0530

    test

--- spec/Polygon_helper.rb
+++ spec/Polygon_helper.rb
@@ -1,11 +1,11 @@
-require_relative "Perimeter"
+require_relative "Polygon"
 require "test/unit"
  
 class TestSimpleNumber < Test::Unit::TestCase
  
   def test_simple
-    assert_equal(4, Triangle.new().add(2) )
-    assert_equal(6, SimpleNumber.new(2).multiply(3) )
+    assert_equal(12, Triangle.new().perimeter([3,4,5]) )
+    assert_equal(10, Rectangle.new().perimeter([2,3,2,3]) )
   end
  
 end
